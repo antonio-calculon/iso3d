320475f7b085459d0a56285f9ac00eafd33d0a5b
cedric <cedric@loulou.home>
2017-04-30 17:57:51 +0200

    initial commit

---
+++ iso3d/iso3d.hpp
@@ -0,0 +1,7 @@
+
+#ifndef _ISO3D_HPP_
+#define _ISO3D_HPP_
+
+#include "iso3d/isobase.hpp"
+
+#endif
---
+++ iso3d/isobase.hpp
@@ -0,0 +1,8 @@
+//
+
+#ifndef _ISOBASE_HPP_
+#define _ISOBASE_HPP_
+
+#include <allegro5/allegro.h>
+
+#endif
